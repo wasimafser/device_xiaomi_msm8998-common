f56b5e26e4c243a283f244ae7ca68acbc689cf97
wasimafser <wasim.afser@gmail.com>
2019-10-06 17:40:35 +0530

    initial commit

---
+++ recovery/Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := recovery_updater.cpp
+LOCAL_MODULE := librecovery_updater_xiaomi
+#LOCAL_MODULE_TAGS := eng
+LOCAL_STATIC_LIBRARIES := libedify libotautil
+include $(BUILD_STATIC_LIBRARY)

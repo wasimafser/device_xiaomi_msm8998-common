bd43c9b2cd8a748d917059f02ad52777d5f04a52
wasimafser <wasim.afser@gmail.com>
2020-03-07 12:39:18 +0530

    Fixed for building PA: * enable vendor move * kernel permissive for now * PA includes legacy-um.mk don't explicitly include it

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -15,7 +15,7 @@
 #
 
 BOARD_VENDOR := xiaomi
-
+PRODUCT_VENDOR_MOVE_ENABLED := true
 PLATFORM_PATH := device/xiaomi/msm8998-common
 
 TARGET_SPECIFIC_HEADER_PATH := $(PLATFORM_PATH)/include
@@ -51,6 +51,7 @@ BOARD_KERNEL_CMDLINE := androidboot.console=ttyMSM0 androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8998
@@ -172,6 +173,7 @@ TARGET_USES_MKE2FS := true
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 TARGET_USES_INTERACTION_BOOST := true
+TARGET_USES_NON_LEGACY_POWERHAL := true
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
@@ -196,7 +198,7 @@ TARGET_USES_OLD_MNC_FORMAT := true
 VENDOR_SECURITY_PATCH := 2020-01-01
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+#include device/qcom/sepolicy/legacy-um/legacy-um.mk
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/private

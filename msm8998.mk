c6a53cc40775f8719fc036d4d1e87816a3b8bbb8
wasimafser <wasim.afser@gmail.com>
2020-03-07 12:43:45 +0530

    Fix for building PA: * build btconfigstore * don't exmplixitly build telephony-ext

--- msm8998.mk
+++ msm8998.mk
@@ -144,6 +144,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
+
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
@@ -171,9 +175,9 @@ PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
 # Doze
-PRODUCT_PACKAGES += \
-    XiaomiDoze \
-    XiaomiParts
+# PRODUCT_PACKAGES += \
+#     XiaomiDoze \
+#     XiaomiParts
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -297,7 +301,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.2-service
 
 # Properties
 include $(LOCAL_PATH)/vendor_prop.mk
@@ -367,7 +371,7 @@ PRODUCT_PACKAGES += \
     qti-telephony-utils \
     qti_telephony_utils.xml
 
-PRODUCT_BOOT_JARS += \
+#PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # USB
